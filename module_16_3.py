28fa551440033f0554ac69734bba4d168088af7e
mourygin <55830119+mourygin@users.noreply.github.com>
2024-11-11 17:56:17 +0300

    Add files via upload

---
+++ module_16_3.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+users = {'1': 'Имя: Example, возраст: 18'}
+
+@app.get("/")
+async def get_userlist() -> dict:
+    return users
+
+@app.post('/user/{username}/{age}')
+async def add_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+                   age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]) -> str:
+    user_id = str(int(max(users, key=int)) + 1)
+    users[user_id] = f'Имя: {username}, возраст: {age}'
+    return f'User {user_id} is registered'
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='11')],
+                      username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+                      age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]) -> str:
+    users[user_id] = f'Имя: {username}, возраст: {age}'
+    return f'User {user_id} is updated'
+
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='11')]) -> str:
+    del users[user_id]
+    return f'User {user_id} has been deleted'
