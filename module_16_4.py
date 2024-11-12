ec59b3178a933c319f347542a30a499c4b6f6ca6
mourygin <55830119+mourygin@users.noreply.github.com>
2024-11-12 15:25:28 +0300

    Add files via upload

---
+++ module_16_4.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, Path, HTTPException
+from typing import Annotated, List
+from pydantic import BaseModel
+
+app = FastAPI()
+
+users = []
+
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
+
+    # def __init__(self, user_id, username, age):
+    #     self.user_id = user_id
+    #     self.username = username
+    #     self.age = age
+
+@app.get("/users")
+async def get_userlist() -> List[User]:
+    return users
+
+@app.post('/user/{username}/{age}')
+async def add_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+                   age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]) -> User:
+    user_id = len(users) #50:30
+    user = User(id=user_id,username=username,age=age)
+    users.append(user)
+    return user
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='11')],
+                      username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+                      age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]) -> str:
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail='User not found')
+
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='11')]) -> List[User]:
+    for user in users:
+        if user.id == user_id:
+            users.remove(user_id)
+            return users
+    raise HTTPException(status_code=404, detail='User not found')
