41ae67587c40396bbc77049cb8ea4ffb6348a177
mourygin <55830119+mourygin@users.noreply.github.com>
2024-11-13 19:49:03 +0300

    Add files via upload

--- module_16_2.py
+++ module_16_2.py
@@ -15,7 +15,7 @@ async def admin_page() -> dict:
 async def user_page(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='11')]) -> dict:
     return {'message': f'Вы вошли как пользователь № {user_id}'}
 
-@app.get('/user')
+@app.get('/user/{username}/{age}')
 async def user_info(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
                     age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]) -> dict:
     return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
